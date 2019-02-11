275cd1808555dc334eea511818686f7c387ada13
kevinaubriet <kevin97@orange.fr>
2019-02-11 15:53:41 +0100

    quest2 terminée

--- src/fr/unice/miage/MyFile.java
+++ src/fr/unice/miage/MyFile.java
@@ -1,4 +1,53 @@
 package fr.unice.miage;
 
+import java.io.File;
+
 public class MyFile {
+
+    private File file;
+
+    public MyFile() {
+        this.file = new File(".");
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    private void listFile(){
+        for (String elem:this.file.list()) {
+            System.out.println(elem);
+        }
+    }
+
+    private void listFile2(File chemin){
+        for (File elem:chemin.listFiles()) {
+            if(elem.isDirectory()){
+                listFile2(elem);
+                System.out.println(elem);
+            }else{
+                System.out.println(elem);
+            }
+
+        }
+    }
+
+
+    public static void main(String[] args){
+
+
+        MyFile fileTest =  new MyFile();
+
+        fileTest.listFile();
+        System.out.println("\n------------------------------\n");
+        fileTest.listFile2(fileTest.getFile());
+
+
+
+
+    }
 }
